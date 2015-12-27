f2a387dd751273ade40fd098f1928b3af053de35
Yutaka Kamei <kamei@ykamei.net>
2015-12-27 23:12:34 +0900

    Add documentation in src/lib.rs

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a URL parsing library written in Rust.
 The goal of this project is to provide simple parsing URL library
 like urllib.parse in Python3.x.
 
-## Example
+## Examples
 
 ### Source code
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 /* Copyright (C) 2015 Yutaka Kamei */
 
+//! This is a URL parsing library.
+//!
+//! The goal of this project is to provide simple parsing URL library
+//! like urllib.parse in Python3.x.
+//!
+//! # Examples
+//!
+//! ```
+//! use urlparse::urlparse;
+//! use urlparse::GetQuery;  // Trait
+//!
+//! let url = urlparse("http://www.example.com/foo?bar=123&col=println%21%28%22TEST%21%22%29&col=sub");
+//! let query = url.get_parsed_query().unwrap();
+//! assert_eq!(url.scheme, "http");
+//! assert_eq!(query.get_first_from_str("col").unwrap(), "println!(\"TEST!\")");
+//! ```
+
 mod quote;
 mod unquote;
 mod query_string;
